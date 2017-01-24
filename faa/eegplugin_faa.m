d84ad5a18d2309e10386164532e5b5576272d21f
Michael Tesař <michael.tesar@nudz.cz>
2017-01-24 10:58:26 +0100

    Update eegplugin_faa.m

--- faa/eegplugin_faa.m
+++ faa/eegplugin_faa.m
@@ -1,4 +1,5 @@
-% eegplugin_pca() - a pca plug-in
+% This book/study is a result of the research funded by the project 
+% Nr. LO1611 with a financial support from the MEYS under the NPU I program.
 function eegplugin_faa(fig, try_strings, catch_strings)
     % Create menu
     toolsmenu = findobj(fig, 'tag', 'tools');
